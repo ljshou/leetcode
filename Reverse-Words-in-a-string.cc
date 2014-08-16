16d4d5255d65b2075f99c0bdb7cd9848942f0f57
ljshou <shoulinjun@126.com>
2014-08-16 18:09:41 +0800

    modify Reverse-words-in-a-string.cc

--- Reverse-Words-in-a-string.cc
+++ Reverse-Words-in-a-string.cc
@@ -6,15 +6,20 @@ public:
         for(auto x : s) {
             if(x == ' ') {
                 if(!word.empty()) {
-                    res = ' ' + word + res;
+                    reverse(word.begin(), word.end());
+                    res += word + ' ';
                     word.clear();
                 }
                 continue;
             }
             word.push_back(x);
         }
-        if(!word.empty()) res = ' ' + word + res; // do not forget
-        if(!res.empty()) res.erase(res.begin());
+        if(!word.empty()) { //do not forget
+          reverse(word.begin(), word.end());
+          res += word + ' ';
+        }
+        if(!res.empty()) res.pop_back();
+        reverse(res.begin(), res.end());
         res.swap(s);
     }
 };
