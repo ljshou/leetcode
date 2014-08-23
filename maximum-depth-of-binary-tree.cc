2cb920aacbe878d59c81e82e3709d016076ce08b
ljshou <shoulinjun@126.com>
2014-08-23 22:17:49 +0800

    add another couple of files

--- maximum-depth-of-binary-tree.cc
+++ maximum-depth-of-binary-tree.cc
@@ -9,33 +9,28 @@
  */
 class Solution {
 public:
-    int minDepth(TreeNode *root) {
+    int maxDepth(TreeNode *root) {
         if(root == nullptr) return 0;
-        //leaf node
         if(root->left == nullptr && root->right == nullptr) return 1;
-        if(root->left && root->right) return 1 + min(minDepth(root->left), minDepth(root->right));
-        if(root->left) return 1 + minDepth(root->left);
-        else return 1 + minDepth(root->right);
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
 
+//非递归
 class Solution {
 public:
-    int minDepth(TreeNode *root) {
+    int maxDepth(TreeNode *root) {
         if(root == nullptr) return 0;
-        queue<TreeNode*> cur, next;
         int level(0);
-        
+        queue<TreeNode*> cur, next;
         cur.push(root);
+        
         while(!cur.empty()) {
             root = cur.front(); cur.pop();
-            if(root->left == nullptr && root->right == nullptr) {
-                return level+1;
-            }
             if(root->left) next.push(root->left);
             if(root->right) next.push(root->right);
             if(cur.empty()) {
-                ++level;
+                ++ level;
                 cur.swap(next);
             }
         }
